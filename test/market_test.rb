b6c6bc4b6a5b78fb4031552966b0fd6f5b542935
John Kim <abcdefghijohnk@gmail.com>
2020-07-29 10:07:37 -0600

    attributes test pass

--- test/market_test.rb
+++ test/market_test.rb
@@ -10,5 +10,7 @@ class MarketTest < Minitest::Test
     market = Market.new("South Pearl Street Farmers Market")
 
     assert_instance_of Market, market
+    assert_equal "South Pearl Street Farmers Market", market.name
+    assert_equal [], market.vendors
   end
 end
