61633a811f0fc4ca6cb179c1ee846f354aa3ace1
John Kim <abcdefghijohnk@gmail.com>
2020-07-29 09:23:54 -0600

    exist and attribute test for vendor pass

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -5,12 +5,20 @@ require './lib/vendor'
 
 class VendorTest < Minitest::Test
 
-  def test_it_exists_and_has_attributes
+  def test_item_exists_and_has_attributes
     item1 = Item.new({name: 'Peach', price: "$0.75"})
     item2 = Item.new({name: 'Tomato', price: '$0.50'})
 
     assert_instance_of Item, item1
     assert_equal "Tomato", item2.name
-    assert_equal 0.5, item2.price 
+    assert_equal 0.5, item2.price
+  end
+
+  def test_vendor_exists_and_has_attributes
+    vendor = Vendor.new("Rocky Mountain Fresh")
+
+    assert_instance_of Vendor, vendor
+    assert_equal "Rocky Mountain Fresh", vendor.name
+    assert_equal ({}), vendor.inventory 
   end
 end
